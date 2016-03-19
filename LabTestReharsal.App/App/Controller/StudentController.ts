72015155ab731af1fe9f4a9da82932f7184ee9d0
Syed Mohammad Hassan <hassan.imroze@hotmail.com>
2016-03-19 12:49:10 +0600

    home 1

--- LabTestReharsal.App/App/Controller/StudentController.ts
+++ LabTestReharsal.App/App/Controller/StudentController.ts
@@ -2,22 +2,32 @@
     export class StudentController {
 
         Student: Student;
+        ErrorOrSuccessList: string[];
+        statusClass: string;
+
         private studentService: StudentService;
+
         static $inject = ["StudentService"];
-        //static $inject = ["$stateProvider", "$urlRouterProvider"];
+
         constructor(studentSvc: StudentService) {
             this.studentService = studentSvc;
         }
 
         Save() {
+            this.ErrorOrSuccessList = new Array<string>();
             console.log(this.Student);
             var self = this;
 
             var successCallback = result => {
                 console.log(result);
+                self.Student = new Student(); 
+                self.statusClass = "successList";
+                self.ErrorOrSuccessList.push("Saved Successfully");
             };
             var errorCallback = error => {
                 console.log(error);
+                self.statusClass = "errorList";
+                self.ErrorOrSuccessList.push(error.data);
             };
 
             self.studentService.Save(self.Student)
